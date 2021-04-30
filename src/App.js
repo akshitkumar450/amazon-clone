ff3aa521998d90c4af909fa9f13577de877f152d
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-30 19:57:30 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
-    <div className="App">
-      <h1>amazon clone</h1>
+    <div className="app">
+      <Header />
+      {/*Home*/}
     </div>
   );
 }
