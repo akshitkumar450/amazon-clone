fcc2e0eb8473f0e0981489916bc0d4e7c622c8da
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-30 23:00:19 +0530

    checkout page and starting of context api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path='/' component={Home} exact={true} />
+          <Route path='/checkout' component={Checkout} exact={true} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Subtotal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Subtotal.css'
+import CurrencyFormat from "react-currency-format";
+function Subtotal() {
+    return (
+        <div className='subtotal'>
+            <CurrencyFormat
+                renderText={(value) => (
+                    <React.Fragment>
+                        <p>
+                            {/* Part of the homework */}
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </React.Fragment>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+
+            <button >Proceed to Checkout</button>
+        </div>
+    )
+}
+
+export default Subtotal
